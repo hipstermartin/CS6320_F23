0b22fb382dc75b05f0a6f7acef1c03d657bacf79
hipstermartin <abhi.yalamaddi@gmail.com>
2023-10-05 20:54:47 -0500

    updated preprocessing

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -1,8 +1,8 @@
 from models import preprocessing, ngram, evaluation
 
 def main():
-    train_tokens = preprocessing.tokenize_text("data/train.txt")
-    val_tokens = preprocessing.tokenize_text("data/val.txt")
+    train_tokens = preprocessing.preprocess_file("data/train.txt")
+    val_tokens = preprocessing.preprocess_file("data/val.txt")
     
     unigram_freq = ngram.compute_unigram_freq(train_tokens)
     bigram_freq = ngram.compute_bigram_freq(train_tokens, unigram_freq)
@@ -13,26 +13,11 @@ def main():
     
     unsmoothed_perplexity = evaluation.compute_perplexity(val_tokens, unigram_freq, bigram_freq, V)
     laplace_perplexity = evaluation.compute_perplexity(val_tokens, unigram_freq, bigram_freq, V, 'adjusted_laplace_smoothing')
-    add_k_perplexity = evaluation.compute_perplexity(val_tokens, unigram_freq, bigram_freq, V, 'adjusted_add_k_smoothing', k=1)
+    add_k_perplexity = evaluation.compute_perplexity(val_tokens, unigram_freq, bigram_freq, V, 'adjusted_add_k_smoothing', k=0.1)
     
     print(f"Unsmoothed: {unsmoothed_perplexity:.2f}")
     print(f"Laplace Smoothed: {laplace_perplexity:.2f}")
     print(f"Add-k Smoothed: {add_k_perplexity:.2f}")
-
-    # Experiment with different vocabulary sizes by adjusting min_freq
-    # for min_freq in [100, 200, 500, 1000]:
-    #     trimmed_unigram_freq = ngram.compute_unigram_freq(train_tokens, min_freq=min_freq)
-    #     trimmed_vocab_size = len(trimmed_unigram_freq)
-        
-    #     # Handle unseen words based on the trimmed vocabulary
-    #     trimmed_val_tokens = preprocessing.handle_unseen_words(val_tokens, set(trimmed_unigram_freq.keys()))
-        
-    #     # Compute and print perplexity for different smoothing methods
-    #     perplexity_unsmoothed = evaluation.compute_perplexity(trimmed_val_tokens, trimmed_unigram_freq, bigram_freq, trimmed_vocab_size)
-    #     perplexity_laplace = evaluation.compute_perplexity(trimmed_val_tokens, trimmed_unigram_freq, bigram_freq, trimmed_vocab_size, smoothing_method='adjusted_laplace_smoothing')
-    #     perplexity_add_k = evaluation.compute_perplexity(trimmed_val_tokens, trimmed_unigram_freq, bigram_freq, trimmed_vocab_size, smoothing_method='adjusted_add_k_smoothing')
-        
-    #     print(f"min_freq: {min_freq}, Unsmoothed Perplexity: {perplexity_unsmoothed}, Laplace Smoothed Perplexity: {perplexity_laplace}, Add-k Smoothed Perplexity: {perplexity_add_k}")
 
 if __name__ == "__main__":
     main()
--- Assignment1/models/preprocessing.py
+++ Assignment1/models/preprocessing.py
@@ -1,11 +1,21 @@
-def tokenize_text(file_path):
-    with open(file_path, 'r') as file:
-        content = file.readlines() 
-    tokens = []
-    for line in content:
-        line_tokens = line.lower().split()
-        tokens.append(line_tokens)
+import re
+def preprocess_text(text):
+    text = text.lower()
+    text = re.sub(r'\d+', '<NUM>', text)
+    text = re.sub(r'([.,!?;])', r' \1 ', text)
+    text = re.sub(r'http\S+', '<URL>', text)
+    text = re.sub(r'\S+@\S+', '<EMAIL>', text)
+    tokens = text.split()
+    tokens = ['<REVIEW_START>'] + tokens + ['<REVIEW_END>']
     return tokens
+
+def preprocess_file(file_path):
+    with open(file_path, 'r') as file:
+        content = file.readlines()
+    
+    preprocessed_reviews = [preprocess_text(review) for review in content]
+    return preprocessed_reviews
+
 
 def handle_unseen_words(tokens, vocab):
     return [[token if token in vocab else "<UNK>" for token in review] for review in tokens]
