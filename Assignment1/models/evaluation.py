a6a3222dddd31d7e8a8ef14da50ea62e9dc5befd
hipstermartin <abhi.yalamaddi@gmail.com>
2023-10-08 17:02:31 -0500

    added missing steps in evaluation

--- Assignment1/models/evaluation.py
+++ Assignment1/models/evaluation.py
@@ -6,6 +6,7 @@ def compute_perplexity(tokens, unigram_freq, bigram_freq, V, smoothing_method=No
     log_prob_sum = 0
     
     for review in tokens:
+        each_review_prob = 1.0
         for i in range(1, len(review)):
             if smoothing_method == 'adjusted_laplace_smoothing':
                 prob = adjusted_laplace_smoothing(review[i-1], review[i], unigram_freq, bigram_freq, V)
@@ -14,11 +15,11 @@ def compute_perplexity(tokens, unigram_freq, bigram_freq, V, smoothing_method=No
             else:
                 prob = bigram_freq.get((review[i-1], review[i]), 0) / unigram_freq.get(review[i-1], 1)
 
-            if prob == 0:
-                prob = 1e-10
-
-            log_prob_sum += -log(prob)
+            each_review_prob *= prob
+        
+        each_review_prob = max(each_review_prob, 1e-20)
+        log_prob_sum += log(each_review_prob)
     
-    avg_neg_log_prob = log_prob_sum / N
+    avg_neg_log_prob = (-1 * log_prob_sum) / N
     perplexity = exp(avg_neg_log_prob)
     return perplexity
